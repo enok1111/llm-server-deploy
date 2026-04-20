f9d33917842ce2bd2b40d00471024f99cd378f9b
benya7 <bitup.games@gmail.com>
2026-04-20 15:48:03 +0200

    update

--- src/start-server.sh
+++ src/start-server.sh
@@ -11,11 +11,10 @@ MODEL_PATH="$MODELS_DIR/$MODEL_FILENAME"
 
 nohup "$SERVER_BIN" \
   -m "$MODEL_PATH" \
-  -c 196608 \
+  -c 262144 \
   -ngl 99 \
   -fa 1 \
-  -np 1 \
-  -sm row \
+  --parallel 1 \
   -ctk q4_0 \
   -ctv q4_0 \
   -t 16 \
@@ -23,6 +22,6 @@ nohup "$SERVER_BIN" \
   -ub 2048 \
   --port 8080 \
   --host 0.0.0.0 \
-  > "$LOG_FILE" 2>&1 &
+  > server.log 2>&1 &
 
 echo $! > "$BASE_DIR/server.pid"
