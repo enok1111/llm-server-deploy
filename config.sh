1c3579497cf89e2a63612fd42237c50a16a5c7b3
benya7 <bitup.games@gmail.com>
2026-05-14 15:53:52 +0200

    try

--- config.sh
+++ config.sh
@@ -15,9 +15,9 @@ MODEL_FILENAME="Qwen3.6-27B-Uncensored.gguf"
 VISION_MODEL_URL="https://huggingface.co/HauhauCS/Qwen3.6-27B-Uncensored-HauhauCS-Balanced/resolve/main/mmproj-Qwen3.6-27B-Uncensored-HauhauCS-Balanced-f16.gguf?download=true"
 VISION_MODEL_FILENAME="mmproj-F16.gguf"
 
-CTX_CHECKPOINTS=32
-CXT_SIZE=262144
-BATCH_SIZE=1024
+CTX_CHECKPOINTS=72
+CXT_SIZE=229376
+BATCH_SIZE=2048
 PORT=8080
 API_KEY="master-api-key-enok1111"
 IDLE_TIMEOUT=1800
