284e6dd9e87667f09b93cc6eb41010109eafe668
benya7 <bitup.games@gmail.com>
2026-05-14 06:43:37 +0200

    feat: configure custom chat template for llama-server

--- src/start-server.sh
+++ src/start-server.sh
@@ -30,6 +30,7 @@ nohup "$SERVER_BIN" \
   --temp "$TEMPERATURE" \
   --no-webui \
   --jinja \
+  --chat-template-file "$BASE_DIR/src/chat-template.jinja" \
   --ctx-checkpoints "$CTX_CHECKPOINTS" \
   --top-p "$TOP_P" \
   --min-p "$MIN_P" \
