e3e9ae5676b1e8406af3102db95a991098747240
LJ <bitup.games@gmail.com>
2026-06-30 21:41:52 +0200

    update: add Qwen3.6-27B-Heretic-Uncensored-FINETUNE-NEO-CODE-Di-IMatrix-MAX

--- config.sh
+++ config.sh
@@ -9,10 +9,10 @@ LLAMA_DIR="$BASE_DIR/llama.cpp"
 LOG_FILE="$BASE_DIR/server.log"
 PID_FILE="$BASE_DIR/server.pid"
 
-MODEL_URL="https://huggingface.co/HauhauCS/Qwen3.6-27B-Uncensored-HauhauCS-Balanced/resolve/main/Qwen3.6-27B-Uncensored-HauhauCS-Balanced-Q4_K_P.gguf?download=true"
-MODEL_FILENAME="Qwen3.6-27B-Uncensored.gguf"
+MODEL_URL="https://huggingface.co/DavidAU/Qwen3.6-27B-Heretic-Uncensored-FINETUNE-NEO-CODE-Di-IMatrix-MAX-GGUF/resolve/main/Qwen3.6-27B-NEO-CODE-HERE-2T-OT-Q4_K_M.gguf?download=true"
+MODEL_FILENAME="Qwen3.6-27B-heretic.gguf"
 
-VISION_MODEL_URL="https://huggingface.co/HauhauCS/Qwen3.6-27B-Uncensored-HauhauCS-Balanced/resolve/main/mmproj-Qwen3.6-27B-Uncensored-HauhauCS-Balanced-f16.gguf?download=true"
+VISION_MODEL_URL="https://huggingface.co/DavidAU/Qwen3.6-27B-Heretic-Uncensored-FINETUNE-NEO-CODE-Di-IMatrix-MAX-GGUF/resolve/main/mmproj-F16.gguf?download=true"
 VISION_MODEL_FILENAME="mmproj-F16.gguf"
 
 CTX_CHECKPOINTS=48
